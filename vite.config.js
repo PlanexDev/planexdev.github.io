2451d5f4d7d59ba29e4cb7a2f3c2b351d7f56a20
PlanexDev <planexdev@gmail.com>
2022-07-14 22:29:02 +0000

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import pkg from "./package.json" assert { type: "json" };
 
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
+
+  ssr: {
+    noExternal: Object.keys(pkg.dependencies || {}),
+  },
 };
 
 export default config;
