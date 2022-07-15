ccb853a9030c834f9d2f2d6635d0d74d3820aabb
PlanexDev <planexdev@gmail.com>
2022-07-15 17:23:21 +0000

    seo

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,27 @@
   import Nav from "$lib/Nav.svelte";
 </script>
 
+<svelte:head>
+  <!-- Primary Meta Tags -->
+  <title>PlanexDev | Frontend Web Developer</title>
+  <meta name="title" content="PlanexDev | Frontend Web Developer">
+  <meta name="description" content="I'm a self-taught middle-school coder who's really interested in web development.">
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://planexdev.github.io/">
+  <meta property="og:title" content="PlanexDev | Frontend Web Developer">
+  <meta property="og:description" content="I'm a self-taught middle-school coder who's really interested in web development.">
+  <meta property="og:image" content="/cover.png">
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="https://planexdev.github.io/">
+  <meta property="twitter:title" content="PlanexDev | Frontend Web Developer">
+  <meta property="twitter:description" content="I'm a self-taught middle-school coder who's really interested in web development.">
+  <meta property="twitter:image" content="/cover.png">
+</svelte:head>
+
 <Nav />
 
 <slot />
