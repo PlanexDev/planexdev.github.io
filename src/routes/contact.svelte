fb2a2de6e96a5b077e4be98f8f31ed2155b2eb95
PlanexDev <planexdev@gmail.com>
2022-07-16 10:09:35 -0700

    format weirdly formatted tags

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -9,11 +9,13 @@
 <p>Here are my social media accounts:</p>
 
 <div class="accounts-wrapper">
-  <a href="https://github.com/PlanexDev"><FontAwesome icon={faGithub} size="3x" color="#aaa" /></a>
+  <a href="https://github.com/PlanexDev">
+    <FontAwesome icon={faGithub} size="3x" color="#aaa" />
+  </a>
 
-  <a href="https://discordapp.com/users/698189280090259476"
-    ><FontAwesome icon={faDiscord} size="3x" color="#aaa" /></a
-  >
+  <a href="https://discordapp.com/users/698189280090259476">
+    <FontAwesome icon={faDiscord} size="3x" color="#aaa" />
+  </a>
 </div>
 
 <style lang="scss">
